3450ec441b4859edac24de03b660fd531f00dacf
EC2 Default User <ec2-user@ip-172-31-36-55.ec2.internal>
2021-08-10 14:42:20 +0000

    Initial Commit

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,19 +1,20 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book.id)
+    #redirect_to book_path(book.id)
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    #redirect_to book_path(params[:book_id])
   end
   
   private
-
+  
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
